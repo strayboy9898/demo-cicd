b514a4050bf421f8477771f35ee77184b97a77b0
trung9898 <maithanhtrung9898@gmail.com>
2022-05-17 21:14:34 +0700

    first commit

---
+++ src/test/java/com/example/demoDocker/DemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
